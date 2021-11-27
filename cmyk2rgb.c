98b25ee1f38513679c092b9801b160034d043707
Laurovič <94954906+Laurovic@users.noreply.github.com>
2021-11-27 13:04:21 +0100

    Update cmyk2rgb.c

--- cmyk2rgb.c
+++ cmyk2rgb.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
     float cyan, magenta, yellow, black;
 
     printf("Zadaj C (0-100):");
     scanf("%f", &cyan);
-    printf("Zadaj M (0-100):");
-    scanf("%f", &magenta);
-    printf("Zadaj Y (0-100):");
-    scanf("%f", &yellow);
-    printf("Zadaj K (0-100):");
-    scanf("%f", &black);
 
-    if (cyan*(cyan-100) <= 0 && magenta*(magenta-100) <= 0 && yellow*(yellow-100) <= 0 && black*(black-100)<= 0){
-        float red = 255*(1-cyan/100)*(1-black/100);
-        float green = 255*(1-magenta/100)*(1-black/100);
-        float blue = 255*(1-yellow/100)*(1-black/100);
+    if (cyan*(cyan-100) <= 0){
+        printf("Zadaj M (0-100):");
+        scanf("%f", &magenta);
+        if (magenta*(magenta-100) <= 0){
+            printf("Zadaj Y (0-100):");
+            scanf("%f", &yellow);
+            if (yellow*(yellow-100) <= 0){
+                printf("Zadaj K (0-100):");
+                scanf("%f", &black);
+                if (black*(black-100) <= 0){
+                    float red = 255*(1-cyan/100)*(1-black/100);
+                    float green = 255*(1-magenta/100)*(1-black/100);
+                    float blue = 255*(1-yellow/100)*(1-black/100);
 
-        printf(" %.0f\n %.0f\n %.0f\n", red, green, blue);
-    } else{
+                    printf(" %.0f\n %.0f\n %.0f\n", red, green, blue);
+                    exit(0);}
+                printf("Zly vstup");
+                exit(0);}
+            printf("Zly vstup");
+            exit(0);}
         printf("Zly vstup");
-    }
-}
+        exit(0);}
+    printf("Zly vstup");
+    exit(0);}
