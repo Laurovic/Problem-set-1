49f6994363043bb2d625b6770f8846ca9ada9553
Laurovič <94954906+Laurovic@users.noreply.github.com>
2021-11-28 17:46:20 +0100

    Update sucinSucetRozdielDelenieZaokruhli.c

--- sucinSucetRozdielDelenieZaokruhli.c
+++ sucinSucetRozdielDelenieZaokruhli.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void){
     int cislo1, cislo2;
-    
+
     printf ("Zadaj prve cislo:");
     scanf ("%i", &cislo1);
-
     printf ("Zadaj druhe cislo:");
     scanf ("%i", &cislo2);
 
-    if (cislo1 > cislo2)
-    {
+    if (cislo1 > cislo2){
         printf ("%i\n", cislo1*cislo2);
         printf ("%i\n", cislo1+cislo2);
         printf ("%i\n", cislo1-cislo2);
-        printf ("%f\n", (float)cislo1/(float)cislo2);
-        printf ("%.0f\n", (float)cislo1/(float)cislo2);
-    }
-    else
-    {
+        if (cislo1 == 0 || cislo2 == 0){
+            printf("0\n0");
+        } else {
+            printf ("%f\n", (float)cislo1/(float)cislo2);
+            printf ("%.0f\n", (float)cislo1/(float)cislo2);
+        }
+    }else{
         printf ("%i\n", cislo2*cislo1);
         printf ("%i\n", cislo2+cislo1);
         printf ("%i\n", cislo2-cislo1);
-        printf ("%f\n", (float)cislo2/(float)cislo1);
-        printf ("%.0f\n", (float)cislo2/(float)cislo1);
-    }
-
-}
+        if (cislo1 == 0 || cislo2 == 0){
+            printf("0\n0");
+        } else {
+            printf ("%f\n", (float)cislo1/(float)cislo2);
+            printf ("%.0f\n", (float)cislo1/(float)cislo2);
+        }}}
