04e728bd987a0ea3bf9c88e52c294efa8bc91254
Laurovič <94954906+Laurovic@users.noreply.github.com>
2021-12-01 08:16:04 +0100

    Update sucinSucetRozdielDelenieZaokruhli.c

--- sucinSucetRozdielDelenieZaokruhli.c
+++ sucinSucetRozdielDelenieZaokruhli.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
     int cislo1, cislo2;
@@ -9,22 +10,24 @@ int main(void){
     scanf ("%i", &cislo2);
 
     if (cislo1 > cislo2){
+        float podiel = (float)cislo1/(float)cislo2;
         printf ("%i\n", cislo1*cislo2);
         printf ("%i\n", cislo1+cislo2);
         printf ("%i\n", cislo1-cislo2);
         if (cislo1 == 0 || cislo2 == 0){
             printf("0\n0");
         } else {
-            printf ("%f\n", (float)cislo1/(float)cislo2);
-            printf ("%.0f\n", (float)cislo1/(float)cislo2);
+            printf ("%f\n", podiel);
+            printf ("%.0f\n", round(podiel));
         }
     }else{
+        float podiel = (float)cislo1/(float)cislo2;
         printf ("%i\n", cislo2*cislo1);
         printf ("%i\n", cislo2+cislo1);
         printf ("%i\n", cislo2-cislo1);
         if (cislo1 == 0 || cislo2 == 0){
             printf("0\n0");
         } else {
-            printf ("%f\n", (float)cislo1/(float)cislo2);
-            printf ("%.0f\n", (float)cislo1/(float)cislo2);
+            printf ("%f\n", podiel);
+            printf ("%.0f\n", round(podiel));
         }}}
